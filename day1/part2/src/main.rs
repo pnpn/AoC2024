3b5e8fee74f9c4cc92b5de0cb413c99a92a7f2fc
Pierre-Nicolas FALCOZ <pnfalcoz@tilakhealthcare.com>
2024-12-01 20:34:31 +0100

    Unmut part2

--- day1/part2/src/main.rs
+++ day1/part2/src/main.rs
@@ -3,7 +3,6 @@ use std::fs::read_to_string;
 fn main() {
     let mut left_list: Vec<u64> = Vec::new();
     let mut right_list: Vec<u64> = Vec::new();
-    let mut occurences: Vec<u64> = Vec::new();
 
     for line in read_to_string("../input.txt").unwrap().lines() {
         for (index, part) in line.split_whitespace().enumerate() {
@@ -15,10 +14,41 @@ fn main() {
         }
     }
 
-    for num in left_list.iter() {
-        let occurence = right_list.iter().filter(|i| **i == *num).count();
-        occurences.push(num * (occurence as u64));
-    }
+    let path = format!("{}/../input.txt", env!("CARGO_MANIFEST_DIR"));
+    let binding = read_to_string(path).unwrap();
+    let lines: Vec<&str> = binding.lines().collect();
+    println!("{}", lines.first().unwrap());
+    let left: Vec<u64> = lines
+        .clone()
+        .iter()
+        .map(|line| {
+            (**line)
+                .split_whitespace()
+                .next()
+                .unwrap()
+                .to_owned()
+                .parse::<u64>()
+                .unwrap()
+        })
+        .collect();
+    let right: Vec<u64> = lines
+        .clone()
+        .iter()
+        .map(|line| {
+            (**line)
+                .split_whitespace()
+                .nth(1)
+                .unwrap()
+                .to_owned()
+                .parse()
+                .unwrap()
+        })
+        .collect();
+
+    let occurences: Vec<u64> = left
+        .iter()
+        .map(|elem| *elem * (right.iter().filter(|i| **i == *elem).count()) as u64)
+        .collect();
 
     let res: u64 = occurences.iter().sum();
 
