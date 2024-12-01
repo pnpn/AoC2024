164d29b789aba14c27683c1a4dd1a4690f249e89
Pierre-Nicolas FALCOZ <pnfalcoz@tilakhealthcare.com>
2024-12-01 19:54:36 +0100

    Unmut result vec

--- day1/part1/src/main.rs
+++ day1/part1/src/main.rs
@@ -10,7 +10,6 @@ fn distance(a: u64, b: u64) -> u64 {
 fn main() {
     let mut left_list: Vec<u64> = Vec::new();
     let mut right_list: Vec<u64> = Vec::new();
-    let mut distances: Vec<u64> = Vec::new();
 
     for line in read_to_string("../input.txt").unwrap().lines() {
         for (index, part) in line.split_whitespace().enumerate() {
@@ -25,12 +24,12 @@ fn main() {
     left_list.sort();
     right_list.sort();
 
-    let it = left_list.iter().zip(right_list.iter());
+    let dist: Vec<u64> = left_list
+        .iter()
+        .zip(right_list.iter())
+        .map(|(elem_left, elem_right)| distance(*elem_left, *elem_right))
+        .collect();
 
-    for (left, right) in it {
-        distances.push(distance(*left, *right));
-    }
-
-    let res: u64 = distances.iter().sum();
+    let res: u64 = dist.iter().sum();
     print!("{}", res)
 }
